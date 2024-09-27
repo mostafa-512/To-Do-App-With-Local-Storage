663e5046dbe22e2f755b5e3851ab6027f31ec3a5
Mostafa Abdalraheem Abosreea Mohammed <161928438+mostafa-512@users.noreply.github.com>
2024-09-27 21:20:11 +0300

    Add some enhancement
    Add some enhancement with two conditions and best practice of code " The Last Commit Come Soon "

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-function addTask() {
-    let x = document.getElementById("input").value; 
-    if (x === '') {
-        alert("You Must Write Something !!");
-    } else {
-        let i = 1;
-            let taskSpace = document.getElementById("taskSection");
-            console.log("Task added: ", x);
-            let li = document.createElement("li");
-            li.innerHTML = x;
-            taskSpace.appendChild(li);
-            
-        }
-        
-    
-    }    
-    
+    let tasks = [];
 
+function addTask() {
+    let inputField = document.getElementById("input");
+    let taskValue = inputField.value.trim(); //trim for remove any spaces
     
+    if (taskValue === '') {
+        alert("You Must Write Something !!");
+        return;
+    }
+    
+    if (tasks.length >= 7) {
+        alert("Sorry, the maximum number of tasks is 7. Finish a One task at least before adding more.");
+        return;
+    }
+    let notfind = tasks.indexOf(taskValue);    
+    if (notfind != -1) {
+        alert(`You Add This Task   [${taskValue}]  Before !!`)
+        return;
+    }
+
+    let taskSpace = document.getElementById("taskSection");
+    let li = document.createElement("li");
+    li.innerHTML = taskValue;
+    taskSpace.appendChild(li);
+    
+    tasks.push(taskValue);
+    console.log("Task added: ", taskValue);
+    console.log("Current tasks: ", tasks);
+    
+    inputField.value = '';
+}
